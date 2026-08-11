cf7904dd16732c43479f4857011db39ea729b64d
Devilkiss <sbqqqiiiu2@163.com>
2026-08-11 08:55:36 +0800

    feat(search): 明星模型类

---
+++ app/src/main/java/com/github/tvbox/osc/search/Celebrity.java
@@ -0,0 +1,19 @@
+package com.github.tvbox.osc.search;
+
+public class Celebrity {
+    public String name;
+    public String py;
+    public String pyi;
+    public String poster;
+
+    public Celebrity() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+}
